7da1a23ff8e96d6dd1d00f2f06fcb72dc8271201
Ariela Israel <arielaisrael12@gmail.com>
2023-10-22 02:53:37 -0400

    dishes tests passing

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -10,6 +10,57 @@ const nextId = require('../utils/nextId');
 ////////////////////////////////////////////////
 // --------------- MIDDLEWARE --------------- //
 ////////////////////////////////////////////////
+const bodyDataHas = propertyName => {
+	return (req, res, next) => {
+		const { data = {} } = req.body;
+		if (data[propertyName]) {
+			return next();
+		}
+		next({ status: 400, message: `Must include a ${propertyName}` });
+	};
+};
+
+const priceIsValidNumber = (req, res, next) => {
+	const { data: { price } = {} } = req.body;
+	if (price < 0 || !Number.isInteger(price)) {
+		return next({
+			status: 400,
+			message: 'price',
+		});
+	}
+	next();
+};
+
+const dishExists = (req, res, next) => {
+	const { dishId } = req.params;
+	const foundDish = dishes.find(dish => dish.id === dishId);
+	if (foundDish) {
+		res.locals.dish = foundDish;
+		return next();
+	}
+	next({
+		status: 404,
+		message: `Dish does not exist: ${dishId}.`,
+	});
+};
+
+const dishIdMatch = (req, res, next) => {
+	const { dishId } = req.params;
+	const { data: { id } = {} } = req.body;
+
+	if (id === undefined || id === '' || id === null) {
+		return next();
+	}
+
+	if (id !== dishId) {
+		return next({
+			status: 400,
+			message: `Dish id does not match route id. Dish: ${id}, Route: ${dishId}`,
+		});
+	}
+
+	next();
+};
 
 ////////////////////////////////////////////////
 // ------------- ROUTE HANDLERS ------------- //
@@ -17,9 +68,61 @@ const nextId = require('../utils/nextId');
 // LIST route handler
 const list = (req, res) => res.json({ data: dishes });
 
+// CREATE route handler
+const create = (req, res) => {
+	const { data: { name, description, price, image_url } = {} } = req.body;
+
+	// NEW DISH creation
+	const newDish = {
+		id: nextId(),
+		name,
+		description,
+		price,
+		image_url,
+	};
+	dishes.push(newDish);
+	res.status(201).json({ data: newDish });
+};
+
+// READ route handler
+const read = (req, res) => res.json({ data: res.locals.dish });
+
+// UPDATE route handler
+const update = (req, res) => {
+	const dish = res.locals.dish;
+	const { data: { name, description, price, image_url } = {} } = req.body;
+
+	// DISH update
+	dish.name = name;
+	dish.description = description;
+	dish.price = price;
+	dish.image_url = image_url;
+
+	res.json({ data: dish });
+};
+
 ////////////////////////////////////////////////
 // ------------- MODULE EXPORTS ------------- //
 ////////////////////////////////////////////////
 module.exports = {
 	list,
+	create: [
+		bodyDataHas('name'),
+		bodyDataHas('description'),
+		bodyDataHas('price'),
+		bodyDataHas('image_url'),
+		priceIsValidNumber,
+		create,
+	],
+	read: [dishExists, read],
+	update: [
+		dishExists,
+		dishIdMatch,
+		bodyDataHas('name'),
+		bodyDataHas('description'),
+		bodyDataHas('price'),
+		bodyDataHas('image_url'),
+		priceIsValidNumber,
+		update,
+	],
 };
--- src/dishes/dishes.router.js
+++ src/dishes/dishes.router.js
@@ -1,11 +1,17 @@
-const router = require("express").Router();
+const router = require('express').Router();
 
 // TODO: Implement the /dishes routes needed to make the tests pass
 // dishes controller
-const controller = require('./dishes.controller')
+const controller = require('./dishes.controller');
 // methodNotAllowed error handler
-const methodNotAllowed = require('../errors/methodNotAllowed')
+const methodNotAllowed = require('../errors/methodNotAllowed');
 
-router.route('/').get(controller.list).all(methodNotAllowed)
+router
+	.route('/')
+	.get(controller.list)
+	.post(controller.create)
+	.all(methodNotAllowed);
+
+router.route('/:dishId').get(controller.read).put(controller.update).all(methodNotAllowed);
 
 module.exports = router;
